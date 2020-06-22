31386124de76cd2804200d5720432d5194158fd1
zoupeng <416323907@qq.com>
2020-06-22 23:15:41 +0800

    33333

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
-
+console.log(666)
 // 引入axios
 import "./api/index.js"
 
